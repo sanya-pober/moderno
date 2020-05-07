e946127e4be76a2927a53b2778b0bbf840e8485d
sanya_pober <poberezhny52@gmail.com>
2020-05-07 14:32:59 +0300

    added product-slider and  product

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,40 @@
 $(function() {
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+    });
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [{
+                breakpoint: 1900,
+                settings: {
+                    dots: true,
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 1441,
+                settings: {
+                    dots: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 801,
+                settings: {
+                    dots: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    });
 
+    var mixer = mixitup('.products__inner-box');
 });
