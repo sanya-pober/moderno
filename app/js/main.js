2f45b3c149ada6ea38763ef7c5385e1d0eac980f
sanya_pober <poberezhny52@gmail.com>
2020-05-10 17:21:59 +0300

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -56,5 +56,13 @@ $(function() {
         $('.icon-th-list').removeClass('active');
     });
 
+    $('.menu__btn').on('click', function() {
+        $('.menu__list').slideToggle();
+    });
+
+    $('.header__btn-menu').on('click', function() {
+        $('.header__box').toggleClass('active');
+    });
+
     var mixer = mixitup('.products__inner-box');
 });
